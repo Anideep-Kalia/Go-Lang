c45cdc0a5fd00c56ec7896571919eb3b3b0db274
Anideep <anideepkalia@gmail.com>
2024-06-03 23:52:24 +0530

    today's teachin

---
+++ golang/22bitmorejson/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	// so the backticks is presentation name so this will be shown as a result when json is made but in development the original name is only used
+	Name     string `json:"coursename"`
+	Price    int
+	Platform string   `json:"website"`
+	Password string   `json:"-"`              // this means that don't show in the document
+	Tags     []string `json:"tags,omitempty"` // this signifies that if the valeue is null then don't show it
+}
+
+func main() {
+	fmt.Println("Welcome to JSON video")
+	// EncodeJson()
+	DecodeJson()
+}
+
+func EncodeJson() {
+
+	lcoCourses := []course{
+		{"ReactJS Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"full-stack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "hit123", nil},
+	}
+
+	//package this data as JSON data
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", finalJson)
+
+}
+
+func DecodeJson() {
+	// saving json in variable
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"website": "LearnCodeOnline.in",
+		"tags": ["web-dev","js"]
+	}
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v  and value is %v and Type is: %T\n", k, v, v)
+	}
+
+}
