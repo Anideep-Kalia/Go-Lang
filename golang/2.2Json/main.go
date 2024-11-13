65ed147505a564ca0086b32d9902a7c830b25e21
Anideep-Kalia <anideepkalia.com>
2024-11-14 00:58:23 +0530

    WORK IN PORGRESS

--- golang/2.2Json/main.go
+++ golang/2.2Json/main.go
@@ -37,9 +37,8 @@ func EncodeJson() {
 	fmt.Printf("%s\n", finalJson)
 
 }
-
 func DecodeJson() {
-	// saving json in variable
+	// example of data coming from API
 	jsonDataFromWeb := []byte(`
 	{
 		"coursename": "ReactJS Bootcamp",
@@ -48,21 +47,15 @@ func DecodeJson() {
 		"tags": ["web-dev","js"]
 	}
 	`)
-
 	var lcoCourse course
-
 	checkValid := json.Valid(jsonDataFromWeb)
-
 	if checkValid {
-		fmt.Println("JSON was valid")
 		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
 		fmt.Printf("%#v\n", lcoCourse)
 	} else {
 		fmt.Println("JSON WAS NOT VALID")
 	}
-
 	// some cases where you just want to add data to key value
-
 	var myOnlineData map[string]interface{}
 	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
 	fmt.Printf("%#v\n", myOnlineData)
